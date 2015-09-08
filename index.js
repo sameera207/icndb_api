665f831e826b11519fabdf998da65d54166cbf6b
sameera <sameera207@gmail.com>
2015-09-08 17:53:34 +1000

    adding tests

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+var request = require('request');
+
 module.exports = {
   random: function(){
-    return 'abc';
+    request.get('http://api.icndb.com/jokes/random', function (error, response, body)     {
+        if (!error && response.statusCode == 200) {
+          console.log(body);
+        }
+    })
   }
 }
+
+request.get("http://api.icndb.com/jokes/random",
+    function(error, response, body) {
+      j = JSON.parse(body)
+      console.log(j['value']['joke'])
+    }
+)
